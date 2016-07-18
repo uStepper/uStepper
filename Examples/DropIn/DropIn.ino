638b1034f2f1f02877b7ca7079a8ddfe56c8abdf
Thomas Olsen <thomas@ustepper.com>
2016-07-18 19:28:01 +0200

    modified some comments and added dropin example

---
+++ Examples/DropIn/DropIn.ino
@@ -0,0 +1,25 @@
+/*
+*			DropIn example sketch!
+*
+*		IMPORTANT! READ THIS !!!
+*
+*	In order for the drop in feature to work, the user
+*	needs to enable the feature. This is done be uncommenting
+*	the "DROPIN" define in the "conf.h" file of the library.
+*	This file can be found in the src folder of the uStepper
+*	library installation.
+*/
+
+#include <uStepper.h>
+
+uStepper stepper;
+
+void setup(void)
+{
+	stepper.setup();
+}
+
+void loop(void)
+{
+
+}
