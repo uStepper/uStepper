9087acaa738f2012b6f8f834ecd300bda921311d
Thomas Olsen <thomas@ustepper.com>
2016-07-25 23:01:00 +0200

    updated dropIn example

--- Examples/DropIn/DropIn.ino
+++ Examples/DropIn/DropIn.ino
@@ -1,13 +1,15 @@
 /*
 *			DropIn example sketch!
 *
-*		IMPORTANT! READ THIS !!!
-*
-*	In order for the drop in feature to work, the user
-*	needs to enable the feature. This is done be uncommenting
-*	the "DROPIN" define in the "conf.h" file of the library.
-*	This file can be found in the src folder of the uStepper
-*	library installation.
+*	Pin connections:
+*	-------------------------
+*	| Controller | uStepper |
+*	|-----------------------|
+*	|	Enable   |	D2 		|
+*	|	Step     |	D3 		|
+*	|	Dir      |	D4 		|
+*	|	GND      |	GND		|
+*	-------------------------	
 */
 
 #include <uStepper.h>
@@ -16,7 +18,7 @@ uStepper stepper;
 
 void setup(void)
 {
-	stepper.setup();
+	stepper.setup(DROPIN,SIXTEEN,3000.0,10);			//Activate dropin, 1/16th microstepping, speed during fault: 3000 steps/s, # of missed steps allowed
 	Serial.begin(115200);
 }
 
