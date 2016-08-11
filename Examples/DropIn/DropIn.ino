3bf4c70f7af762fe31d7b5f7ecbdc8701a86a8fd
Thomas Olsen <thomas@ustepper.com>
2016-08-11 20:32:40 +0200

    changed definitions of external interrupt routines

--- Examples/DropIn/DropIn.ino
+++ Examples/DropIn/DropIn.ino
@@ -16,13 +16,13 @@
 *	please make sure to check this.
 */
 
-#include <uStepper.h>
+//#include <uStepper.h>
 
-uStepper stepper;
+//uStepper stepper;
 
 void setup(void)
 {
-	stepper.setup(DROPIN,SIXTEEN,3000.0,20);			//Activate dropin, 1/16th microstepping, speed during fault: 3000 steps/s, # of missed steps allowed
+	//stepper.setup(DROPIN,SIXTEEN,3000.0,20);			//Activate dropin, 1/16th microstepping, speed during fault: 3000 steps/s, # of missed steps allowed
 }
 
 void loop(void)
